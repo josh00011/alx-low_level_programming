9cda9d941cef51bbf2dd76596fe8ccf0de6a19d1
Josh <jklordney@gmail.com>
2022-10-17 22:43:58 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = '0' num <= '9'; num++)
 	{
 		putchar(num);
 		if (num == '9')
@@ -18,7 +18,7 @@ int main(void)
 		else
 		{
 			putchar(',');
-			putchar('');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
